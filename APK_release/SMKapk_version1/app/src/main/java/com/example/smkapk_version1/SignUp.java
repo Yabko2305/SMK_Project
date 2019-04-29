3d6fb1408299bfe1d5c35dfa6025974ca8889b09
Yabko2305 <z.yarko2305@gmail.com>
2019-04-29 21:00:54 +0300

    29.04 Patch 1.1 , fixed bugs

---
+++ APK_release/SMKapk_version1/app/src/main/java/com/example/smkapk_version1/SignUp.java
@@ -0,0 +1,70 @@
+package com.example.smkapk_version1;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        final EditText name = findViewById(R.id.nameSignUpEditText);
+        final EditText surname = findViewById(R.id.SurnameSignUpEditText);
+        final EditText email = findViewById(R.id.EmailSignUpEditText);
+        final EditText password = findViewById(R.id.PasswordSignUpEditText);
+        final EditText repeatpassword = findViewById(R.id.RepeatPasswordSignUpEditText);
+        final TextView wrongArguments = findViewById(R.id.wrongArgumentsSignUp);
+        final TextView wrongPasswords = findViewById(R.id.wrongPasswordsSignUp);
+        wrongArguments.setVisibility(View.INVISIBLE);
+        wrongPasswords.setVisibility(View.INVISIBLE);
+
+        Button signup = findViewById(R.id.SignUpButton);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!name.getText().toString().equals("") && !surname.getText().toString().equals("") &&!email.getText().toString().equals("") &&!password.getText().toString().equals("") &&!repeatpassword.getText().toString().equals(""))
+                {
+                    if(password.getText().toString().equals(repeatpassword.getText().toString()))
+                    {
+                        UserClass newUser = new UserClass();
+                        newUser.name = name.getText().toString();
+                        newUser.surname = surname.getText().toString();
+                        newUser.email = email.getText().toString();
+                        newUser.password = password.getText().toString();
+                        MainActivity.users.add(newUser);
+                        Intent i = new Intent(getApplicationContext() , MainActivity.class);
+                        startActivity(i);
+                    }
+                    else
+                    {
+                        wrongPasswords.setVisibility(View.VISIBLE);
+                        Handler handler = new Handler();
+                        handler.postDelayed(new Runnable() {
+                            public void run() {
+                                wrongPasswords.setVisibility(View.INVISIBLE);
+                            }
+                        }, 2000);
+                    }
+                }
+                else
+                {
+                    wrongArguments.setVisibility(View.VISIBLE);
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        public void run() {
+                            wrongArguments.setVisibility(View.INVISIBLE);
+                        }
+                    }, 2000);
+                }
+            }
+        });
+    }
+}
---
+++ APK_release/SMKapk_version1/app/src/main/java/com/example/smkapk_version1/UserClass.java
@@ -0,0 +1,8 @@
+package com.example.smkapk_version1;
+
+public class UserClass {
+    public String name;
+    public String surname;
+    public String email;
+    public String password;
+}
