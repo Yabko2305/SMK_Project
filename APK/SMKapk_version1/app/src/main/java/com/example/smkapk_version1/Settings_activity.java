f2139d45c315609dfc5e2cb0cac8cbc2635d4f1b
Yabko2305 <z.yarko2305@gmail.com>
2019-05-01 12:14:55 +0300

    30.04 Features and bugfixes, read PatchNotes

--- APK/SMKapk_version1/app/src/main/java/com/example/smkapk_version1/Settings_activity.java
+++ APK/SMKapk_version1/app/src/main/java/com/example/smkapk_version1/Settings_activity.java
@@ -2,6 +2,9 @@ package com.example.smkapk_version1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.MenuItem;
@@ -11,36 +14,21 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.widget.TextView;
 
-public class HomePage_Activity extends AppCompatActivity
+public class Settings_activity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout_of_slideout_menu);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        setContentView(R.layout.activity_settings_activity);
+        Toolbar toolbar = findViewById(R.id.toolbar_settings);
         setSupportActionBar(toolbar);
+
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        TextView nameview = findViewById(R.id.NameShowScrollActivity);
-
-
-
-        Intent pastIntent = getIntent();
-        int num = pastIntent.getIntExtra("Number", -10);
-        if (num != -1) {
-            nameview.setText(LogIn_Activity.users.get(num).name + " " + LogIn_Activity.users.get(num).surname);
-
-        } else {
-            nameview.setText("Yaroslav" + " " + "Zhyhailo");
-
-        }
-
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout_settings);
+        NavigationView navigationView = findViewById(R.id.nav_view_settings);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -50,7 +38,7 @@ public class HomePage_Activity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout_settings);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -66,6 +54,7 @@ public class HomePage_Activity extends AppCompatActivity
     }
 
 
+
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -73,19 +62,19 @@ public class HomePage_Activity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_home) {
-            Intent home = new Intent(getApplicationContext() , HomePage_Activity.class);
-            startActivity(home);
+            Intent inte = new Intent(getApplicationContext() , HomePage_Activity.class);
+            inte.putExtra("Number" , -1 );
+            startActivity(inte);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_tools)
-        {
-            Intent inte = new Intent(getApplicationContext() , Settings_activity.class);
-            startActivity(inte);
+        } else if (id == R.id.nav_tools) {
+            //do nothing
         }
-            DrawerLayout drawer = findViewById(R.id.drawer_layout);
-            drawer.closeDrawer(GravityCompat.START);
-            return true;
-        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout_settings);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
+}
